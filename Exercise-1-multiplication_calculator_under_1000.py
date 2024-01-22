10faf11f0e3a299b8b96c094142d8c52e4a71566
unknown <miggy.payumo26@gmail.com>
2024-01-23 02:55:59 +0800

    Input working but still accepts string values

--- Exercise-1-multiplication_calculator_under_1000.py
+++ Exercise-1-multiplication_calculator_under_1000.py
@@ -4,3 +4,15 @@
 # 1. Create a multiplication calculator
 # 2. Product must be under 1000
 # 3. If the product is higher than 1000, return the sum
+
+
+# Multiplication Calculator
+
+# while True:
+    first_value = input ("Enter your first integer:")
+    second_value = input ("Enter your second integer:")
+    if first_value.isdigit and second_value.isdigit():
+        first_value = int
+        second_value = int 
+    else:
+        print("Values must be numbers!")
